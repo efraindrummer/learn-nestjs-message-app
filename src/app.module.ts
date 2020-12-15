7cc8560d84d56facf149dfbe362f1d1df3f19262
efraindrummer <efrain_may_75@hotmail.com>
2020-12-14 19:34:47 -0600

    inico backend con nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MensajesController } from './mensajes/mensajes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest',
+      password: 'app',
+      database: 'messageapp',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController, MensajesController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/mensajes/mensajes.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { CreateMensajeDto } from './dto/create-mensaje-dto';
+
+@Controller('mensajes')
+export class MensajesController {
+
+
+    @Post()
+    create(@Body() crateMensajeDto: CreateMensajeDto){
+        return 'mensaje creado';
+    }
+
+    @Get()
+    getAll(){
+        return 'lista de mensajes';
+    }
+
+    @Put(':id')
+    update(@Body() updateMensajeDto: CreateMensajeDto){
+        return 'mensaje actualizado';
+    }
+
+    @Delete(':id')
+    delete(){
+        return 'Mensaje eliminado';
+    }
+}
